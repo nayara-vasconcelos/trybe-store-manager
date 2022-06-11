ca077dcea25c4e8e74816049ef7c35c5c15cf222
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-06-11 02:37:35 -0300

    feat: faz a query para listar os produtos do BD

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,1 +1,14 @@
-// Renomeie esse arquivo
+const connection = require('./connection');
+
+const getAll = async () => {
+  const query = `SELECT * FROM StoreManager.products
+  ORDER BY id ASC`;
+
+  // execResult = [[result], [buffer]]
+  const [products] = await connection.execute(query);
+  return products;
+};
+
+module.exports = {
+  getAll,
+};
