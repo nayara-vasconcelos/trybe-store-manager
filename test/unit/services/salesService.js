d99a448ba12ccaf793da85b9ee20fa74cb5a483c
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-06-11 08:53:43 -0300

    test: testa getAll e getById de salesService

---
+++ test/unit/services/salesService.js
@@ -0,0 +1,141 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const salesModel = require('../../../models/salesModel');
+const { getAll, getById } = require('../../../services/salesService');
+
+describe('Ao chamar getAll do salesModel', () => {
+  describe('quando não há vendas cadastradas', () => {
+    const resultModel = [];
+
+    before(() => {
+      sinon.stub(salesModel, 'getAll')
+        .resolves(resultModel);
+    });
+
+    after(() => {
+      salesModel.getAll.restore();
+    });
+
+    it('retorna um array', async () => {
+      const result = await getAll();
+      expect(result).to.be.an('array');
+    });
+
+    it('o array está vazio', async () => {
+      const result = await getAll();
+      expect(result).to.be.empty;
+    });
+  });
+
+  describe('quando há vendas cadastradas', () => {
+    const resultModel = [
+      {
+        saleId: 1,
+        date: '2021-09-09T04:54:29.000Z',
+        productId: 1,
+        quantity: 2
+      },
+      {
+        saleId: 1,
+        date: '2021-09-09T04:54:54.000Z',
+        productId: 2,
+        quantity: 2
+      },
+    ];
+
+    before(() => {
+      sinon.stub(salesModel, 'getAll')
+        .resolves(resultModel);
+    });
+
+    after(() => {
+      salesModel.getAll.restore();
+    });
+
+    it('retorna um array', async () => {
+      const result = await getAll();
+      expect(result).to.be.an('array');
+    });
+
+    it('o array não está vazio', async () => {
+      const result = await getAll();
+      expect(result).to.not.be.empty;
+    });
+
+    it('o array contém objetos', async () => {
+      // result = array[0]
+      const [result] = await getAll();
+      expect(result).to.be.an('object');
+    });
+
+    it('cada objeto possui as propriedades "saleId", "date", "productId" e "quantity"', async () => {
+      // result = array[0]
+      const [result] = await getAll();
+      expect(result).to.include.all.keys('saleId', 'date', 'productId', 'quantity');
+    });
+  });
+});
+
+describe('Ao chamar getById do salesModel', () => {
+  describe('quando não há venda com determinado id', () => {
+    const resultModel = null;
+    const invalidID = 5;
+
+    before(() => {
+      sinon.stub(salesModel, 'getById')
+        .resolves(resultModel);
+    });
+
+    after(() => {
+      salesModel.getById.restore();
+    });
+
+    it('retorna null', async () => {
+      const result = await getById(invalidID);
+      expect(result).to.be.null;
+    });
+  });
+
+  describe('quando há venda com determinado id', () => {
+    const validId = 1;
+    const resultModel = [
+      {
+        date: '2021-09-09T04:54:29.000Z',
+        productId: 1,
+        quantity: 2
+      },
+      {
+        date: '2021-09-09T04:54:54.000Z',
+        productId: 2,
+        quantity: 2
+      },
+    ];
+
+    before(() => {
+      sinon.stub(salesModel, 'getById')
+        .resolves(resultModel);
+    });
+
+    after(() => {
+      salesModel.getById.restore();
+    });
+
+    it('retorna um array', async () => {
+      const result = await getById(validId);
+      expect(result).to.be.an('array');
+    });
+
+    it('o array contém objetos', async () => {
+      // result = array[0]
+      const [result] = await getById(validId);
+      expect(result).to.be.an('object');
+    });
+
+    it('cada objeto possui as propriedades "date", "productId" e "quantity"', async () => {
+      // result = array[0]
+      const [result] = await getById(validId);
+      expect(result).to.include.all.keys('date', 'productId', 'quantity');
+    });
+  });
+});
