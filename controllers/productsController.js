c82ca119394f9dc6d5d923b5b35d0bfcee86ce75
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-06-11 02:50:28 -0300

    feat: adiciona middleware para a rota GET /products

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,1 +1,11 @@
-// Renomeie esse arquivo
+const productsService = require('../services/productsService');
+const { statusCodes: { OK } } = require('../constants/status');
+
+const getAll = async (_req, res) => {
+  const products = await productsService.getAll();
+  return res.status(OK).json(products);
+};
+
+module.exports = {
+  getAll,
+};
