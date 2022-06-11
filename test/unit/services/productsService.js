164f910c1140eb5d188ac137b650ac8f1cd8089f
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-06-11 02:38:49 -0300

    test: testa getAll de productsService

---
+++ test/unit/services/productsService.js
@@ -0,0 +1,107 @@
+// Retornar todos os produtos em GET /products
+// Se não houver produtos cadastrados:
+// retorná um array vazio.
+// Se houver produtos cadastrados:
+// retornará um array de objetos.
+// o array será ordenado pelo id
+
+// Retornar um produto em GET /products/:id
+// Se não houver produto cadastrado com id:
+// retornar null
+// Se houver:
+// Retornar um objeto
+// Objeto com as propriedades id, name, quantity
+
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const { getAll } = require('../../../services/productsService');
+const productsModel = require('../../../models/productsModel');
+
+// Função para auxiliar a verificação da ordem do array de produtos;
+// const getIdValues = (arrayOfObjs) => {
+//   const ids = arrayOfObjs.map((obj) => obj.id);
+//   return ids;
+// };
+
+// numbers.sort((a, b) => a - b);
+
+// items.sort(function (a, b) {
+//   return a.value - b.value;
+// });
+
+describe('Ao chamar o getAll do productsService', () => {
+  describe('quando não há produtos cadastrados', () => {
+    const resultModel = [];
+
+    before(() => {
+      sinon.stub(productsModel, 'getAll')
+        .resolves(resultModel);
+    });
+
+    after(() => {
+      productsModel.getAll.restore();
+    });
+
+    it('retorna um array', async () => {
+      const result = await getAll();
+      expect(result).to.be.an('array');
+    });
+
+    it('o array está vazio', async () => {
+      const result = await getAll();
+      expect(result).to.be.empty;
+    });
+  });
+
+  describe('quando há produtos cadastrados', () => {
+    const resultModel = [
+      {
+        id: 1,
+        name: 'produto A',
+        quantity: 10,
+      },
+      {
+        id: 2,
+        name: 'produto B',
+        quantity: 20,
+      }
+    ];
+
+    before(() => {
+      sinon.stub(productsModel, 'getAll')
+        .resolves(resultModel);
+    });
+
+    after(() => {
+      productsModel.getAll.restore();
+    });
+
+    it('retorna um array', async () => {
+      const result = await getAll();
+      expect(result).to.be.an('array');
+    });
+
+    it('o array não está vazio', async () => {
+      const result = await getAll();
+      expect(result).to.not.be.empty;
+    });
+
+    it('o array contém objetos', async () => {
+      const [result] = await getAll();
+      expect(result).to.be.an('object');
+    });
+
+    it('cada objeto possui as propriedades "id", "name" e "quantity"', async () => {
+      const [result] = await getAll();
+      expect(result).to.include.all.keys('id', 'name', 'quantity');
+    });
+
+    // it('o array está ordenado pela propriedade "id" dos objetos em ordem crescente', async () => {
+    //   // result = array[0]
+    //   const [result] = await getAll();
+    //   expect(getIdValues(result)).to.have.ordered.members([1, 2])
+    //     .but.not.have.ordered.members([2, 1]);
+    // });
+  });
+});
