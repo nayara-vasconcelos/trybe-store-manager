821e7971db871946bb78598ffe63b28a5a793f68
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-06-14 05:42:29 -0300

    refactor: getById da camada Controller
    Refatora as funcoes getById da camada Controller para atribuir a
    responsabilidade de tratamento de erro aos middlewares de erro.

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,23 +1,17 @@
 const productsService = require('../services/productsService');
 
-const {
-  statusCodes: { OK, NOT_FOUND },
-  msgStatus: { productNotFound },
-} = require('../constants/status');
+const { OK } = require('../constants/status');
 
 const getAll = async (_req, res) => {
   const products = await productsService.getAll();
   return res.status(OK).json(products);
 };
 
-const getById = async (req, res) => {
+const getById = async (req, res, next) => {
   const { id } = req.params;
   const product = await productsService.getById(id);
 
-  if (!product) {
-    return res.status(NOT_FOUND).json(productNotFound);
-  }
-
+  if (product.error) { return next(product.error); }
   return res.status(OK).json(product);
 };
 
--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,22 +1,17 @@
 const salesService = require('../services/salesService');
 
-const {
-  statusCodes: { OK, NOT_FOUND },
-  msgStatus: { saleNotFound },
-} = require('../constants/status');
+const { OK } = require('../constants/status');
 
 const getAll = async (_req, res) => {
   const sales = await salesService.getAll();
   return res.status(OK).json(sales);
 };
 
-const getById = async (req, res) => {
+const getById = async (req, res, next) => {
   const { id } = req.params;
   const sale = await salesService.getById(id);
 
-  if (!sale) {
-    return res.status(NOT_FOUND).json(saleNotFound);
-  }
+  if (sale.error) { return next(sale.error); }
 
   return res.status(OK).json(sale);
 };
