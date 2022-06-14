4bf671adbb31fa3bfb0cd8b09610e9df576fbbd9
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-06-14 05:33:02 -0300

    refactor: middleware tratamento de erros
    Ajusta o midleware generico para tratamento de erros para
    que ele retorne resposta para erros de validacao e erros de dominio.

--- middlewares/errorMiddlewares.js
+++ middlewares/errorMiddlewares.js
@@ -1,31 +1,39 @@
 const { 
-  statusCode: { 
-    INTERNAL_SERVER_ERROR,
-    BAD_REQUEST,
-    UNPROCESSABLE_ENTITY,
-  },
+  INTERNAL_SERVER_ERROR,
+  BAD_REQUEST,
+  UNPROCESSABLE_ENTITY,
+  NOT_FOUND,
+  CONFLICT,
 } = require('../constants/status');
+
+const statusByErrorCode = {
+  notFound: NOT_FOUND,
+  alreadyExists: CONFLICT,
+};
 
 const handleErrors = (err, _req, res, _next) => {
   if (err.status) {
     return res.status(err.status).json({ message: err.message });
   }
 
+  if (err.code) {
+    const status = statusByErrorCode[err.code] || INTERNAL_SERVER_ERROR;
+    return res.status(status).json({ message: err.message });
+  }
+
   return res.status(INTERNAL_SERVER_ERROR).json({ message: 'Internal Server Error' });
 };
 
 const handleValidationErrors = (err, _req, _res, next) => {
-  console.log('ValidationError - Message: ', err.message, 'Erro: ', err);
-
   if (err.message.match(/required/i)) {
-    next({ status: BAD_REQUEST, message: err.message });
+    return next({ status: BAD_REQUEST, message: err.message });
   }
 
   if (err.message.match(/must be/i)) {
-    next({ status: UNPROCESSABLE_ENTITY, message: err.message });
+    return next({ status: UNPROCESSABLE_ENTITY, message: err.message });
   }
 
-  next(err);
+  return next(err);
 };
 
 module.exports = {
