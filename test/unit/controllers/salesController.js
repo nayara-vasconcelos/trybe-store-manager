efa9bab0cbfd60e7212e0ff9dfb4fbec0bd32287
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-06-16 05:10:23 -0300

    test: adiciona testes para salesController
    Adiciona teste para create, update e deleteById.

--- test/unit/controllers/salesController.js
+++ test/unit/controllers/salesController.js
@@ -1,9 +1,9 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 
-const { getAll, getById } = require('../../../controllers/salesController');
+const { getAll, getById, create, update, deleteById } = require('../../../controllers/salesController');
 const salesService = require('../../../services/salesService');
-const { OK } = require('./status');
+const { OK, CREATED, NO_CONTENT } = require('./status');
 
 describe('Ao chamar getAll do salesController', () => {
   describe('quando não existem produtos cadastrados', () => {
@@ -175,6 +175,144 @@ describe('Ao chamar getById do salesController', () => {
       await getById(request, response);
 
       expect(response.json.calledWith(resultService)).to.be.equal(true);
+    });
+  });
+});
+
+describe('Ao chamar create do salesController', () => {
+  describe('quando o body da requisição possui o formato correto', async () => {
+    const response = {};
+    const request = {};
+    const resultService = {
+      id: 1,
+      itemsSold: [
+        {
+          productId: 1,
+          quantity: 2,
+        },
+        {
+          productId: 2,
+          quantity: 5,
+        },
+      ],
+    };
+
+    before(() => {
+      request.body = [
+        {
+          productId: 1,
+          quantity: 2,
+        },
+        {
+          productId: 2,
+          quantity: 5,
+        },
+      ];
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(salesService, 'create').resolves(resultService);
+    });
+
+    after(() => {
+      salesService.create.restore();
+    });
+
+    it('é chamado o método "status" passando o código http 201', async () => {
+      await create(request, response);
+
+      expect(response.status.calledWith(CREATED)).to.be.equal(true);
+    });
+
+    it('é chamado um método "json" passando um objeto', async () => {
+      await create(request, response);
+
+      expect(response.json.calledWith(sinon.match.object)).to.be.equal(true);
+    });
+
+    it('é chamado um método "json" passando um objeto específico', async () => {
+      await create(request, response);
+
+      expect(response.json.calledWith(resultService)).to.be.equal(true);
+    });
+  });
+});
+
+describe('Ao chamar update do salesController', () => {
+  describe('quando o body da requisição possui o formato correto', async () => {
+    const response = {};
+    const request = {};
+    const resultService = {
+      saleId: 1,
+      itemUpdated: [
+        {
+          productId: 1,
+          quantity: 6,
+        }
+      ],
+    };
+
+    before(() => {
+      request.body = [{ productId: 1, quantity: 6 }];
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(salesService, 'update').resolves(resultService);
+    });
+
+    after(() => {
+      salesService.update.restore();
+    });
+
+    it('é chamado o método "status" passando o código http 200', async () => {
+      await update(request, response);
+
+      expect(response.status.calledWith(OK)).to.be.equal(true);
+    });
+
+    it('é chamado um método "json" passando um objeto', async () => {
+      await update(request, response);
+
+      expect(response.json.calledWith(sinon.match.object)).to.be.equal(true);
+    });
+
+    it('é chamado um método "json" passando um objeto específico', async () => {
+      await update(request, response);
+
+      expect(response.json.calledWith(resultService)).to.be.equal(true);
+    });
+  });
+});
+
+describe('Ao chamar deleteById do salesController', () => {
+  describe('quando há produto cadastrado com determinado id', async () => {
+    const response = {};
+    const request = {};
+    const resultService = true;
+    const validId = '1';
+
+    before(() => {
+      request.params = { id: validId };
+      response.status = sinon.stub().returns(response);
+      response.end = sinon.stub();
+
+      sinon.stub(salesService, 'deleteById').resolves(resultService);
+    });
+
+    after(() => {
+      salesService.deleteById.restore();
+    });
+
+    it('é chamado o método "status" passando o código http 204', async () => {
+      await deleteById(request, response);
+
+      expect(response.status.calledWith(NO_CONTENT)).to.be.equal(true);
+    });
+
+    it('é chamado um método "end" sem parâmetro', async () => {
+      await deleteById(request, response);
+
+      expect(response.end.called).to.be.equal(true);
     });
   });
 });
