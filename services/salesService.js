6322950132e1fbd6e73980d19801b2e07f1e710e
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-06-16 05:23:48 -0300

    feat: implementa funcao para registrar uma venda

--- services/salesService.js
+++ services/salesService.js
@@ -1,4 +1,5 @@
 const salesModel = require('../models/salesModel');
+const salesProductsModel = require('../models/salesProductsModel');
 
 // Outros erros
 const notFoundError = {
@@ -17,7 +18,27 @@ const getById = async (id) => {
   return sale;
 };
 
+const create = async (products) => {
+  const [newSale] = await salesModel.create();
+  await Promise.all(products
+    .map((product) => salesProductsModel
+    .create(newSale.id, product.productId, product.quantity)));
+
+  const createdSale = {
+    id: newSale.id,
+    itemsSold: products,
+  };
+  return createdSale;
+};
+
+const update = async () => {};
+
+const deleteById = async () => {};
+
 module.exports = {
   getAll,
   getById,
+  create,
+  update,
+  deleteById,
 };
